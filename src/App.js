fb3dfa7496e077f7b2fc75813d83c122cf757873
patelshravan <patelshravan24@gmail.com>
2022-04-05 21:42:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="signIn" element={<Login />} />
+        <Route path="/" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
