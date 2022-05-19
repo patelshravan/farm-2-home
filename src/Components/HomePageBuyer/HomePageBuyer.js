4f59814ee7a8107ebc9c393a120dc83090013794
patelshravan <patelshravan24@gmail.com>
2022-05-19 14:10:57 +0530

    changes discarded

--- src/Components/HomePageBuyer/HomePageBuyer.js
+++ src/Components/HomePageBuyer/HomePageBuyer.js
@@ -16,9 +16,6 @@ const HomePageSeller = () => {
           <NavLink className="firstNav" to="/viewItems">
             🕵 View Items
           </NavLink>
-          <NavLink className="firstNav" to="/viewItems">
-            🕵 View Items
-          </NavLink>
           <NavLink className="firstNav" to="/profileBuyer">
             👦 Profile
           </NavLink>
